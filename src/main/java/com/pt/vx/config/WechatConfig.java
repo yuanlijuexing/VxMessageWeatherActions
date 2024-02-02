07c9a90f70349efeeda1ae4340683f9c1a8cb13b
yuanlijuexing <74700594+yuanlijuexing@users.noreply.github.com>
2024-02-02 15:39:34 +0800

    Update WechatConfig.java

--- src/main/java/com/pt/vx/config/WechatConfig.java
+++ src/main/java/com/pt/vx/config/WechatConfig.java
@@ -50,7 +50,7 @@ public class WechatConfig {
         ));
 
        userList.add(getUser(
-                "oksjw6ojQSo9wC8E-cn1-mIvyHZI", //扫码关注你的测试号以后，测试平台会出现TA的微信号
+                "oksjw6q9-absQfXmsw5WFbn3PLs0", //扫码关注你的测试号以后，测试平台会出现TA的微信号
                 "G17iUZ_vsiEUsDD7m8YnViAwl4JNT5B6Gfwvp8OAfdE", //要给这个人发送的模板ID
                 "小吴", //咋称呼这个人
                 "福建省福州市马尾区", //这个人的详细地址
